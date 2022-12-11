dba3e3aae7b00d138ddf4153e5509c46d7ef2cfe
sarvar02 <sarvartulkunov2002@gmail.com>
2022-12-11 21:39:26 +0500

    1) New Entities created and connected with each other. 2) Written a method that accepts infos about surahs from Internet

--- db/src/main/java/io/quran/db/entity/Language.java
+++ db/src/main/java/io/quran/db/entity/Language.java
@@ -1,31 +1,16 @@
 package io.quran.db.entity;
 
-<<<<<<< HEAD
-
 import io.quran.db.entity.template.AbsEntity;
-=======
-import com.fasterxml.jackson.annotation.JsonFormat;
->>>>>>> origin/main
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-<<<<<<< HEAD
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import javax.persistence.*;
-import java.sql.Timestamp;
-=======
-import javax.persistence.*;
-import java.time.Instant;
->>>>>>> origin/main
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-<<<<<<< HEAD
 @Entity(name = ("languages"))
 public class Language extends AbsEntity {
 
@@ -35,19 +20,3 @@ public class Language extends AbsEntity {
     @Column(name = ("code"))
     private String code;
 }
-
-=======
-@Entity(name = "languages")
-public class Language {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    @Column(name = "name")
-    private String name;
-
-    @Column(name = "code")
-    private String code;
-}
->>>>>>> origin/main
