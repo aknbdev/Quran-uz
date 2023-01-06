d3c4a1fb7ff7a8ddd9d352722641734eb9c9df49
Abdulazizkhan.K.N.B <abdulazizkhan.k.n.b@gmail.com>
2023-01-06 16:13:29 +0500

    Added railway server link to the DocConfig

--- quranuz-app/src/main/java/io/quran/app/config/DocConfig.java
+++ quranuz-app/src/main/java/io/quran/app/config/DocConfig.java
@@ -16,7 +16,9 @@ import org.springframework.context.annotation.Configuration;
         version = "0.0.1",
         contact = @Contact(name = "idea one", url = "idea-one.is-a.dev"),
         license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0")),
-        servers = {@Server(url = "http://localhost:8080", description = "Local server"), @Server(url = "https:some/url")})
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Local server"),
+                @Server(url = "https://quranuz-backend.up.railway.app")})
 @SecurityScheme(scheme = "bearer",
         name = ProjectConfig.PROJECT_NAME,
         type = SecuritySchemeType.HTTP,
--- quranuz-core/src/main/java/io/quran/core/config/BeanConfig.java
+++ quranuz-core/src/main/java/io/quran/core/config/BeanConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
 import java.util.List;
 
 @Configuration
