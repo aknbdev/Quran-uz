7f8e6ba210cc9abe2d7835f94827fca6653f1ad9
Abdulazizkhan.K.N.B <abdulazizkhan.k.n.b@gmail.com>
2023-01-06 16:05:49 +0500

    Configured Cors, can request from EveryWhere fixed

--- quranuz-core/src/main/java/io/quran/core/config/BeanConfig.java
+++ quranuz-core/src/main/java/io/quran/core/config/BeanConfig.java
@@ -1,11 +1,13 @@
 package io.quran.core.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
@@ -18,13 +20,15 @@ public class BeanConfig {
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+    public FilterRegistrationBean customCorsFilter() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-        return source;
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
